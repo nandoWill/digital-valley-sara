802566460a9311967b200b2ac0fce197651a23f8
Marilia Cristina <mariliaccviana@alu.ufc.br>
2018-04-06 09:52:02 -0300

    Classe Evento atualizada de acordo com Sistema Chronos

--- Sara/src/br/ufc/russas/model/Evento.java
+++ Sara/src/br/ufc/russas/model/Evento.java
@@ -5,19 +5,59 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Evento {
+	private int ID;
+	private int totalHoras;
 	private String nome;
-	private String site;
+	private String descricao;
+	private String local;
+	private String sigla;
+	private ArrayList<Evento> subAtividades;
 	private ArrayList<Trilha> trilhas;
+	private Evento pai;
 	private LocalDate dataEvento;
-	private File informacoes;
-	private Usuario coordenadorGeral;
+	private Usuario coordenadorEvento;
 	
-	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public int getTotalHoras() {
+		return totalHoras;
+	}
+	public void setTotalHoras(int totalHoras) {
+		this.totalHoras = totalHoras;
+	}
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public ArrayList<Evento> getSubAtividades() {
+		return subAtividades;
+	}
+	public void setSubAtividades(ArrayList<Evento> subAtividades) {
+		this.subAtividades = subAtividades;
 	}
 	public ArrayList<Trilha> getTrilhas() {
 		return trilhas;
@@ -25,30 +65,22 @@ public class Evento {
 	public void setTrilhas(ArrayList<Trilha> trilhas) {
 		this.trilhas = trilhas;
 	}
+	public Evento getPai() {
+		return pai;
+	}
+	public void setPai(Evento pai) {
+		this.pai = pai;
+	}
 	public LocalDate getDataEvento() {
 		return dataEvento;
 	}
 	public void setDataEvento(LocalDate dataEvento) {
 		this.dataEvento = dataEvento;
 	}
-	public File getInformacoes() {
-		return informacoes;
+	public Usuario getCoordenadorEvento() {
+		return coordenadorEvento;
 	}
-	public void setInformacoes(File informacoes) {
-		this.informacoes = informacoes;
+	public void setCoordenadorEvento(Usuario coordenadorEvento) {
+		this.coordenadorEvento = coordenadorEvento;
 	}
-	public String getSite() {
-		return site;
-	}
-	public void setSite(String site) {
-		this.site = site;
-	}
-	public Usuario getCoordenadorGeral() {
-		return coordenadorGeral;
-	}
-	public void setCoordenadorGeral(Usuario coordenadorGeral) {
-		this.coordenadorGeral = coordenadorGeral;
-	}
-	
-	
 }
